200d9a6a38255bc9b840e8ca002945e45e328ad7
Yanze Li <jintiandajin4info@gmail.com>
2021-12-16 14:24:20 -0500

    Challenge 3[Fail] – Google sign in not working, BUT Sign in and Log in with email is working!

--- app/src/main/java/com/example/yanze_li_200411363_final/LoginActivity.kt
+++ app/src/main/java/com/example/yanze_li_200411363_final/LoginActivity.kt
@@ -8,6 +8,10 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 //import com.google.firebase.auth.FirebaseAuth
 //import com.google.firebase.auth.GoogleAuthProvider
 //import com.google.firebase.auth.ktx.auth
@@ -15,121 +19,121 @@ import android.widget.Toast
 //import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
-//    private lateinit var auth: FirebaseAuth
-//    private lateinit var account: EditText
-//    private lateinit var password: EditText
-//    private lateinit var sign_up: Button
-//    private lateinit var log_in: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var account: EditText
+    private lateinit var password: EditText
+    private lateinit var sign_up: Button
+    private lateinit var log_in: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-//        initial_variables()
-//        button_func()
+        initial_variables()
+    }
+
+    fun initial_variables(){
+        auth = Firebase.auth
+        account = findViewById(R.id.account_area)
+        password = findViewById(R.id.password_area)
+        sign_up = findViewById(R.id.sign_up_button)
+        log_in = findViewById(R.id.log_in_button)
+        button_func()
 
     }
 
-//    fun initial_variables(){
-//        auth = Firebase.auth
-//        account = findViewById(R.id.account_area)
-//        password = findViewById(R.id.password_area)
-//        sign_up = findViewById(R.id.sign_up_button)
-//        log_in = findViewById(R.id.log_in_button)
-//    }
-//
-//    //Signup button onClick event
-//    fun button_func() {
-//        log_in.setOnClickListener {
-//
-//            if (TextUtils.isEmpty(account.text.toString().trim()))
-//            {
-//                Toast.makeText(
-//                    applicationContext,
-//                    "Please enter email address first!",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                Log.d("Creation","Enter email first!")
-//            }
-//            else if (TextUtils.isEmpty(password.text.toString().trim()))
-//            {
-//                Toast.makeText(
-//                    applicationContext,
-//                    "Please enter password first!",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                Log.d("Creation","Enter password first!")
-//            }
-//            else
-//            {
-//                val email = account.text.toString().trim()
-//                val password = password.text.toString().trim()
-//
-//                //Add this account to the firebase.
-//                auth = FirebaseAuth.getInstance()
-//                auth.signInWithEmailAndPassword(email, password)
-//                    .addOnCompleteListener(this) { task ->
-//                        if (task.isSuccessful) {
-//                            // Sign in success
-//                            Toast.makeText(applicationContext, "success!", Toast.LENGTH_SHORT).show()
-//                            Log.d("Creation","Success")
-//                            val profile_activity = Intent(this,MainActivity::class.java)
-//                            startActivity(profile_activity)
-//                            this.finish()
-//                        } else {
-//                            // Sign in success
-//                            Toast.makeText(applicationContext, "Fail!", Toast.LENGTH_SHORT).show()
-//                            Log.d("Creation","Fail")
-//                        }
-//                    }
-//
-//            }
-//
-//        }
-//        sign_up.setOnClickListener {
-//            //If either account or password is empty:
-//            if (TextUtils.isEmpty(account.text.toString().trim()))
-//            {
-//                Toast.makeText(
-//                    applicationContext,
-//                    "Please enter email address first!",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                Log.d("Creation","Enter email first!")
-//            }
-//            else if (TextUtils.isEmpty(password.text.toString().trim()))
-//            {
-//                Toast.makeText(
-//                    applicationContext,
-//                    "Please enter password first!",
-//                    Toast.LENGTH_SHORT
-//                ).show()
-//                Log.d("Creation","Enter password first!")
-//            }
-//            else
-//            {
-//                val email = account.text.toString().trim()
-//                val password = password.text.toString().trim()
-//
-//                //Add this account to the firebase.
-//                auth = FirebaseAuth.getInstance()
-//                auth.createUserWithEmailAndPassword(email, password)
-//                    .addOnCompleteListener(this) { task ->
-//                        if (task.isSuccessful) {
-//                            // Sign in success
-//                            Toast.makeText(applicationContext, "success!", Toast.LENGTH_SHORT).show()
-//                            Log.d("Creation","Success")
-//                            val profile_activity = Intent(this,MainActivity::class.java)
-//                            startActivity(profile_activity)
-//                            this.finish()
-//                        } else {
-//                            // Sign in success
-//                            Toast.makeText(applicationContext, "Fail!", Toast.LENGTH_SHORT).show()
-//                            Log.d("Creation","Fail")
-//                        }
-//                    }
-//
-//            }
-//        }
-//    }
+    //Signup button onClick event
+    fun button_func() {
+        log_in.setOnClickListener {
+
+            if (TextUtils.isEmpty(account.text.toString().trim()))
+            {
+                Toast.makeText(
+                    applicationContext,
+                    "Please enter email address first!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d("Creation","Enter email first!")
+            }
+            else if (TextUtils.isEmpty(password.text.toString().trim()))
+            {
+                Toast.makeText(
+                    applicationContext,
+                    "Please enter password first!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d("Creation","Enter password first!")
+            }
+            else
+            {
+                val email = account.text.toString().trim()
+                val password = password.text.toString().trim()
+
+                //Add this account to the firebase.
+                auth = FirebaseAuth.getInstance()
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success
+                            Toast.makeText(applicationContext, "success!", Toast.LENGTH_SHORT).show()
+                            Log.d("Creation","Success")
+                            val profile_activity = Intent(this,MainActivity::class.java)
+                            startActivity(profile_activity)
+                            this.finish()
+                        } else {
+                            // Sign in success
+                            Toast.makeText(applicationContext, "Fail!", Toast.LENGTH_SHORT).show()
+                            Log.d("Creation","Fail")
+                        }
+                    }
+
+            }
+
+        }
+        sign_up.setOnClickListener {
+            //If either account or password is empty:
+            if (TextUtils.isEmpty(account.text.toString().trim()))
+            {
+                Toast.makeText(
+                    applicationContext,
+                    "Please enter email address first!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d("Creation","Enter email first!")
+            }
+            else if (TextUtils.isEmpty(password.text.toString().trim()))
+            {
+                Toast.makeText(
+                    applicationContext,
+                    "Please enter password first!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d("Creation","Enter password first!")
+            }
+            else
+            {
+                val email = account.text.toString().trim()
+                val password = password.text.toString().trim()
+
+                //Add this account to the firebase.
+                auth = FirebaseAuth.getInstance()
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success
+                            Toast.makeText(applicationContext, "success!", Toast.LENGTH_SHORT).show()
+                            Log.d("Creation","Success")
+                            val profile_activity = Intent(this,MainActivity::class.java)
+                            startActivity(profile_activity)
+                            this.finish()
+                        } else {
+                            // Sign in success
+                            Toast.makeText(applicationContext, "Fail!", Toast.LENGTH_SHORT).show()
+                            Log.d("Creation","Fail")
+                        }
+                    }
+
+            }
+        }
+    }
 }
