f42196b824fdc80f562c609cfcd89cbfebef1c81
Yanze Li <jintiandajin4info@gmail.com>
2021-12-16 14:57:21 -0500

    Navigation to add new page!

--- app/src/main/java/com/example/yanze_li_200411363_final/MainActivity.kt
+++ app/src/main/java/com/example/yanze_li_200411363_final/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.yanze_li_200411363_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var add_button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        initial_variable()
+    }
+    fun initial_variable(){
+        add_button = findViewById(R.id.Add_button)
+        button_func()
+    }
+    fun button_func() {
+        add_button.setOnClickListener {
+            val profile_activity = Intent(this,DetailActivity::class.java)
+            startActivity(profile_activity)
+            this.finish()
+        }
     }
 }
